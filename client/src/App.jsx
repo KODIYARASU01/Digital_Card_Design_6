51b74253bf2109ae20e7097e1f8914aa861b913d
KODIYARASU01 <kodiyarasu02gmail.com>
2024-04-29 02:24:17 +0530

    DigitalCard8 design client side finished

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+import NewCardDesign8 from './Component/NewCardDesign8';
+
+const App = () => {
+  return (
+    <div>
+<NewCardDesign8/>
+    </div>
+  )
+}
+
+export default App;
